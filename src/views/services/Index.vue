7a1f646aba7c56180e5c49b3b69786d8b1d32fad
Stuart Laverick <stuart@appropriatesolutions.co.uk>
2020-09-23 17:02:53 +0100

    Added is_national filter to services view

--- src/views/services/Index.vue
+++ src/views/services/Index.vue
@@ -33,6 +33,11 @@
                     <gov-label for="filter[referral_method]">Referral method</gov-label>
                     <gov-select v-model="filters.referral_method" id="filter[referral_method]" name="filter[referral_method]" :options="referralMethods"/>
                   </gov-form-group>
+
+                  <gov-form-group>
+                    <gov-label for="filter[is_national]">National</gov-label>
+                    <gov-select v-model="filters.is_national" id="filter[is_national]" name="filter[is_national]" :options="nationalOptions"/>
+                  </gov-form-group>
                 </template>
               </ck-table-filters>
             </gov-grid-column>
@@ -51,6 +56,7 @@
               { heading: 'Organisation', sort: 'organisation_name' },
               { heading: 'Status' },
               { heading: 'Referral method' },
+              { heading: 'National?' },
             ]"
             :view-route="(service) => {
               return {
@@ -71,6 +77,9 @@
             <template slot="cell:3" slot-scope="{ resource: service }">
               {{ displayReferralMethod(service.referral_method) }}
             </template>
+            <template slot="cell:4" slot-scope="{ resource: service }">
+              {{ service.is_national ? 'Yes' : 'No' }}
+            </template>
           </ck-resource-listing-table>
 
         </gov-grid-column>
@@ -80,58 +89,68 @@
 </template>
 
 <script>
-import CkResourceListingTable from "@/components/Ck/CkResourceListingTable.vue";
-import CkTableFilters from "@/components/Ck/CkTableFilters.vue";
+import CkResourceListingTable from '@/components/Ck/CkResourceListingTable.vue';
+import CkTableFilters from '@/components/Ck/CkTableFilters.vue';
 
 export default {
-  name: "ListServices",
+  name: 'ListServices',
   components: { CkResourceListingTable, CkTableFilters },
   data() {
     return {
       filters: {
-        name: "",
-        organisation_name: "",
-        status: "",
-        referral_method: ""
+        name: '',
+        organisation_name: '',
+        status: '',
+        referral_method: '',
+        is_national: '',
       },
       statuses: [
-        { value: "", text: "All" },
-        { value: "active", text: "Enabled" },
-        { value: "inactive", text: "Disabled" }
+        { value: '', text: 'All' },
+        { value: 'active', text: 'Enabled' },
+        { value: 'inactive', text: 'Disabled' },
       ],
       referralMethods: [
-        { value: "", text: "All" },
-        { value: "internal", text: "Internal" },
-        { value: "external", text: "External" },
-        { value: "none", text: "None" }
-      ]
+        { value: '', text: 'All' },
+        { value: 'internal', text: 'Internal' },
+        { value: 'external', text: 'External' },
+        { value: 'none', text: 'None' },
+      ],
+      nationalOptions: [
+        { value: '', text: 'All' },
+        { value: true, text: 'National' },
+        { value: false, text: 'Local' },
+      ],
     };
   },
   computed: {
     params() {
       const params = {
-        include: "organisation",
-        "filter[has_permission]": true
+        include: 'organisation',
+        'filter[has_permission]': true,
       };
 
-      if (this.filters.name !== "") {
-        params["filter[name]"] = this.filters.name;
+      if (this.filters.name !== '') {
+        params['filter[name]'] = this.filters.name;
       }
 
-      if (this.filters.organisation_name !== "") {
-        params["filter[organisation_name]"] = this.filters.organisation_name;
+      if (this.filters.organisation_name !== '') {
+        params['filter[organisation_name]'] = this.filters.organisation_name;
       }
 
-      if (this.filters.status !== "") {
-        params["filter[status]"] = this.filters.status;
+      if (this.filters.status !== '') {
+        params['filter[status]'] = this.filters.status;
       }
 
-      if (this.filters.referral_method !== "") {
-        params["filter[referral_method]"] = this.filters.referral_method;
+      if (this.filters.referral_method !== '') {
+        params['filter[referral_method]'] = this.filters.referral_method;
+      }
+
+      if (this.filters.is_national !== '') {
+        params['filter[is_national]'] = this.filters.is_national;
       }
 
       return params;
-    }
+    },
   },
   methods: {
     onSearch() {
@@ -139,21 +158,21 @@ export default {
       this.$refs.servicesTable.fetchResources();
     },
     onAddService() {
-      this.$router.push({ name: "services-pre-create" });
+      this.$router.push({ name: 'services-pre-create' });
     },
     displayStatus(status) {
       switch (status) {
-        case "active":
-          return "Enabled";
-        case "inactive":
-          return "Disabled";
+        case 'active':
+          return 'Enabled';
+        case 'inactive':
+          return 'Disabled';
         default:
           return status;
       }
     },
     displayReferralMethod(referralMethod) {
       return referralMethod.charAt(0).toUpperCase() + referralMethod.substr(1);
-    }
-  }
+    },
+  },
 };
 </script>
